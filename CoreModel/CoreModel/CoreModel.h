d52cd841356e19725e5380b2737543bf72225414
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-03-27 13:42:28 -0500

    created CoreModel

---
+++ CoreModel/CoreModel/CoreModel.h
@@ -0,0 +1,19 @@
+//
+//  CoreModel.h
+//  CoreModel
+//
+//  Created by Alsey Coleman Miller on 3/27/15.
+//  Copyright (c) 2015 ColemanCDA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CoreModel.
+FOUNDATION_EXPORT double CoreModelVersionNumber;
+
+//! Project version string for CoreModel.
+FOUNDATION_EXPORT const unsigned char CoreModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreModel/PublicHeader.h>
+
+
