57b1d848988657a99a375aa939e820949ad73969
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-03-27 13:28:40 -0500

    first commit

---
+++ Model Editor/Model Editor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Model Editor
+//
+//  Created by Alsey Coleman Miller on 3/27/15.
+//  Copyright (c) 2015 ColemanCDA. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
